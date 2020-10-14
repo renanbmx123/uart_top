b646555c6e0fefe6be615acbdcb25ddfa195ff2a
vlsi2_g02 (Marlon) <renanbmx123@gmail.com>
2020-10-14 15:09:11 -0300

    Testado as constraints, e gerado os relatorios de area, potencia e tempo.

---
+++ Relatorio/TOP/relatorio.tcl
@@ -0,0 +1,11 @@
+set_db library /soft64/design-kits/stm/65nm-cmos065_536/CORE65GPSVT_5.1/libs/CORE65GPSVT_nom_1.00V_25C.lib
+
+read_hdl -vhdl ./fifo_async.vhd 
+read_hdl -vhdl ./uart_baud_clk.vhd 
+read_hdl -vhdl ./uart_top.vhd 
+
+elaborate UART_TOP
+
+read_sdc ./constraints.sdc
+
+synthesize -to_generic -eff high
